6813b45ce427c07bfca0ab3bb568a8865a12a6e9
Marco Greiveldinger <43136849+marcogreiveldinger@users.noreply.github.com>
2023-08-15 17:55:47 +0800

    add pre_bootstrap script to download yadm and clone the repository

---
+++ pre_bootstrap.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Create directory if it doesn't exist
+mkdir -p ~/.local/bin
+
+# Download yadm
+curl -fLo ~/.local/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm
+
+# Give execute permissions to yadm
+chmod a+x ~/.local/bin/yadm
+
+# Clone dotfiles repository
+~/.local/bin/yadm clone https://github.com/marcogreiveldinger/.dotfiles.git
+
+# Run yadm bootstrap
+~/.local/bin/yadm bootstrap
+
+# Clean up by removing yadm
+rm -rf ~/.local/bin/yadm
+
+echo "Dotfiles setup completed!"
