08be45e5c77ed2a008a020b1d8f606f93d35aba8
Marco Greiveldinger <43136849+marcogreiveldinger@users.noreply.github.com>
2023-08-15 18:37:32 +0800

    change homebrew in pre_bootstrap and bootstrap

--- pre_bootstrap.sh
+++ pre_bootstrap.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 
+# install homebrew first as it installs xcode as well to use git!
+if ! command -v brew &> /dev/null
+then
+    echo 'brew must be installed! (/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)")'
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Homebrew already installed. Getting updates..."
+    brew update
+    brew doctor
+fi
+
 # Create directory if it doesn't exist
 mkdir -p ~/.local/bin
 
 # Download yadm
-curl -fLo ~/.local/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm
+curl -sfLo ~/.local/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm
 
 # Give execute permissions to yadm
 chmod a+x ~/.local/bin/yadm
 
 # Clone dotfiles repository
-~/.local/bin/yadm clone https://github.com/marcogreiveldinger/.dotfiles.git
+~/.local/bin/yadm clone --bootstrap -f https://github.com/marcogreiveldinger/.dotfiles.git
 
 # Run yadm bootstrap
 ~/.local/bin/yadm bootstrap
